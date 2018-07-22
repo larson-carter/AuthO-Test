35ebbbc652c199e1a633c3ad668bf608a10d8dc9
Larson Carter <larsoncarter@blazermail.net>
2018-07-21 21:12:11 -0500

    Completely Working
    This repo is now completely working with signing into the app. Now going to add the profile and other useful features. I am now going to continue to create the other app that I am using this for. Going to add more to the UI/UX to make it more appealing. Going to add more objC support. Compinling in under 11 seconds trying to get it to under 10. Going to add more support in the future for more signins. Adding new features as often as I can.

--- AuthO Test/ViewController.swift
+++ AuthO Test/ViewController.swift
@@ -8,13 +8,73 @@
 
 import UIKit
 
+import Auth0
+
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    @IBAction func showLoginController(_ sender: UIButton) {
+        
+        guard let clientInfo = plistValues(bundle: Bundle.main) else { return }
+        Auth0
+            .webAuth()
+            .scope("openid profile")
+            .audience("https://" + clientInfo.domain + "/userinfo")
+            .start {
+                
+                switch $0 {
+                    
+                case .failure(let error):
+                    print("Error: \(error)")
+                case .success(let credentials):
+                    guard let accessToken = credentials.accessToken else { return }
+                    self.showSuccessAlert(accessToken)
+                    
+                }
+                
+        }
+        
+    }
+    
+    // MARK: - Private
+    fileprivate func showSuccessAlert(_ accessToken: String) {
+        
+        let alert = UIAlertController(title: "Success", message: "accessToken: \(accessToken)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
+func plistValues(bundle: Bundle) -> (clientId: String, domain: String)? {
+    
+    guard
+        let path = bundle.path(forResource: "Auth0", ofType: "plist"),
+        let values = NSDictionary(contentsOfFile: path) as? [String: Any]
+        else {
+            
+            print("Missing Auth0.plist file with 'ClientId' and 'Domain' entries in main bundle!")
+            return nil
+            
+    }
+    
+    guard
+        let clientId = values["ClientId"] as? String,
+        let domain = values["Domain"] as? String
+        else {
+            
+            print("Auth0.plist file at \(path) is missing 'ClientId' and/or 'Domain' entries!")
+            print("File currently has the following entries: \(values)")
+            return nil
+            
+    }
+    
+    return (clientId: clientId, domain: domain)
+    
+}
